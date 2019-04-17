697ad609cae92f7cff9463244cd6fb87493175ec
ximmit <41272171+ximmit@users.noreply.github.com>
2019-04-18 00:47:23 +0300

    new_version

--- youtube_api_v3.py
+++ youtube_api_v3.py
@@ -1,5 +1,5 @@
-api_key="AIzaSyCzk1hhI8kg0HCL1fMHJTQQZf10bgkzDm8"
 from apiclient.discovery import build
+api_key="AIzaSyCzk1hhI8kg0HCL1fMHJTQQZf10bgkzDm8"
 youtube=build('youtube','v3', developerKey=api_key)
 type(youtube)
 req=youtube.search().list(part="snippet",order="viewCount",type='video')
