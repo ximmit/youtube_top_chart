173194c8583c29580dbca08bac6c24ee2bd8973d
ximmit <41272171+ximmit@users.noreply.github.com>
2019-04-20 01:05:59 +0300

    Update youtube_api_v3.py

--- youtube_api_v3.py
+++ youtube_api_v3.py
@@ -1,8 +1,23 @@
 import datetime
+from openpyxl.utils.dataframe import dataframe_to_rows
 from apiclient.discovery import build
+from openpyxl import Workbook
 api_key="AIzaSyCzk1hhI8kg0HCL1fMHJTQQZf10bgkzDm8"
 youtube=build('youtube','v3', developerKey=api_key)
 type(youtube)
-req=youtube.search().list(part="snippet",order="viewCount",type='video',maxResults=1,publishedAfter='2018-06-01T00:00:00Z')
+req=youtube.search().list(part="snippet",order="viewCount",type='video',maxResults=50)
+#req=youtube.videos().list(part="snippet,contentDetails,statistics",chart="mostPopular",maxResults=50)
 res=req.execute()
-print(res)
+#print(res['items'][0])
+i=1
+wb = Workbook()
+ws = wb.active
+for it in res['items']:
+ print(it['snippet']['title'])
+ ws["A"+str(i)].value = it['snippet']['title']
+ ws.cell(row=i, column=2).value ="https: // www.youtube.com / watch?v ="+it['id']['videoId']
+
+ i=i+1
+
+wb.save("youtube_chart.xlsx")
+ #print(it['id']['videoId'])
